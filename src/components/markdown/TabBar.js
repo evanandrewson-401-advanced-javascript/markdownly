e36a499de5f093ed79c1f4bb077fcd055c264f90
evanandrewson <evanandrewson@gmail.com>
2019-11-15 15:08:41 -0800

    refactored Document to use hooks instead of connect

--- src/components/markdown/TabBar.js
+++ src/components/markdown/TabBar.js
@@ -30,7 +30,6 @@ TabBar.propTypes = {
   handleClick: PropTypes.func.isRequired,
   handleAdd: PropTypes.func.isRequired,
   handleDelete: PropTypes.func.isRequired,
-  focus: PropTypes.string.isRequired,
   handleTitle: PropTypes.func.isRequired,
   editTitle: PropTypes.shape({
     editInput: PropTypes.bool.isRequired,
